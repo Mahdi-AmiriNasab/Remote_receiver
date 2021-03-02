1fff75e5987d801413835e305eef67592a13b9e8
Mahdi-AmiriNasab <mehdi.amirinasab@yahoo.com>
2021-03-02 13:00:42 +0330

    data receive is now based on interrupt

--- RemoteReceiver.ino
+++ RemoteReceiver.ino
@@ -6,7 +6,54 @@
 #include "string.h"
 #include "pins_arduino.h"
 
+
+/***************Variables****************/
+int Xaxis = A0;    // select the input pin for the Xaxis Joystick
+int Yaxis = A1;    // select the input pin for the Yazis Joystick
+uint32_t XValue = 0, YValue = 0;    // analog values of axis accordingly 
+char  Buff[20];
+char  received_packet[50];
+char print_pkt[100];
+uint32_t to_print_cntr = 0;
+
+
 /*************Functions*************/
+void rx_int(int data_size)
+{
+  // try to parse packet
+  //int packetSize = LoRa.parsePacket();
+  if (data_size) 
+  {
+    // received a packet
+    Serial.println("Received packet: ");
+
+    char * received_packet_ptr = received_packet;
+    // read packet
+    while (LoRa.available()) {
+      *received_packet_ptr++ = (char)LoRa.read();
+    }
+    *received_packet_ptr = '\0'; //terminator
+
+    LoRa.read(); //flush 
+
+    // print RSSI of packet
+    Serial.print(received_packet);
+        
+    sprintf(print_pkt, "signal strength is: %d dbm", LoRa.packetRssi());
+    Serial.print(print_pkt);
+    Serial.println();
+    to_print_cntr = 0;
+    blink(20, 1);
+  }
+  else
+  {
+   
+    LoRa.read(); //flush 
+    Serial.println("no data in int");
+
+  }
+}
+
 void blink (uint32_t del, uint32_t times)
 {
   for(uint32_t i = 0; i < times; i++)
@@ -22,16 +69,9 @@ void blink (uint32_t del, uint32_t times)
 #define avr_dem   10
 #define pin_CE        53
 #define pin_RST       49  
-#define pin_D0        47
+#define pin_D0        2
 
 
-/***************Variables****************/
-int Xaxis = A0;    // select the input pin for the Xaxis Joystick
-int Yaxis = A1;    // select the input pin for the Yazis Joystick
-uint32_t XValue = 0, YValue = 0;    // analog values of axis accordingly 
-char  Buff[20];
-char  received_packet[50];
-char print_pkt[100];
 
 /*************Initializations*************/
 void setup() 
@@ -59,6 +99,11 @@ void setup()
   LoRa.setSignalBandwidth(500000);
   LoRa.setSpreadingFactor(8);
 
+  LoRa.setGain(6);
+  LoRa.receive();
+  LoRa.onReceive(rx_int);
+  
+
   
   Serial.println("initialization SUCCESSFUL");
   blink(300, 2);
@@ -66,41 +111,18 @@ void setup()
   delay(300);
 }
 
-uint32_t to_print_cntr = 0;
+
 void loop()
 {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) 
+  delay(1);
+  if(to_print_cntr++ > 5000)
   {
-    // received a packet
-    Serial.println("Received packet: ");
-
-    char * received_packet_ptr = received_packet;
-    // read packet
-    while (LoRa.available()) {
-      *received_packet_ptr++ = (char)LoRa.read();
-    }
-    *received_packet_ptr = '\0'; //terminator
-
-    // print RSSI of packet
-    Serial.print(received_packet);
-        
-    sprintf(print_pkt, "signal strength is: %d dbm", LoRa.packetRssi());
-    Serial.print(print_pkt);
-    Serial.println();
-
-    blink(20, 1);
     to_print_cntr = 0;
+    LoRa.read(); //flush 
+    Serial.println("no data"); 
   }
-  else
-  {
-    if(to_print_cntr++ > 10000)
-    {
-      to_print_cntr = 0;
-      Serial.println("no data");
-    }
-  }
-  
 }
+
+
+
 
